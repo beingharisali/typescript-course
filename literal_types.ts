74f1c48e4a500124f041479ef0ede385f1224d9b
beingharisali <theharis99@gmail.com>
2026-04-21 14:35:59 +0500

    added literal and union types

---
+++ literal_types.ts
@@ -0,0 +1,8 @@
+let directions: "Top" | "Right" | "Bottom" | "Left";
+directions = "Top";
+console.log(directions);
+function setColors(colors: "red" | "green" | "blue" | "white") {
+  return colors;
+}
+
+console.log(setColors("blue"));
---
+++ union_types.ts
@@ -0,0 +1,5 @@
+let myVar: string | number;
+myVar = "Hello";
+console.log(myVar);
+myVar = 50;
+console.log(myVar);
