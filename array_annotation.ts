ad840db2b9ba5f4d10d725d92e3493d82e2178c2
beingharisali <theharis99@gmail.com>
2026-04-21 18:48:04 +0500

    multidimensional array annotation

--- array_annotation.ts
+++ array_annotation.ts
@@ -2,3 +2,7 @@ let fruits: string[] = ["apple", "banana", "cherry"];
 for (let i = 0; i < fruits.length; i++) {
   console.log(`Fruit: ${fruits[i].toUpperCase()}`);
 }
+
+let arrayOne: number[] = [1, 2, 3, 4, 5];
+let arrayTwo: string[] = ["A", "B", "C", "D", "E"];
+let arrayThree: (number | string)[] = [1, 2, 3, 4, 5, "A", "B", "C", "D", "E"];
